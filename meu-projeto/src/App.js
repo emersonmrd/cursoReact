b5715d855a44c57f043ae1e731920930dd3f6efa
Emerson Martins <138334126+emersonmrd@users.noreply.github.com>
2024-02-25 13:01:16 -0300

    Aplicando Components, Props e CSS modules

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
+  const nome = 'Maria'
+
   return (
     <div className="App">
-     <h1>Olá React</h1>
-     <p>Meu primeiro app</p>
+    <h1>Testando CSS  </h1>
+    <Frase/>
+    <Frase/>
+    <SayMyName nome="Emerson"/>
+    <SayMyName nome="João"/>
+    <SayMyName nome={nome} />
+    <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
