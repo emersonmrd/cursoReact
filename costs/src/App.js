2b817c76a04e8638fa8f8cb21899221d84278788
Emerson Martins <138334126+emersonmrd@users.noreply.github.com>
2024-03-22 12:38:30 -0300

    Navbar e footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,28 +3,26 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
-      
+      <Navbar></Navbar>
         <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/company" element={<Company />} />
+          <Route exact path="/projects" element={<Projects />} />
           <Route  exact path="/contact" element={<Contact />} />
           <Route  exact path="/newproject" element={<NewProject />} />
         </Routes>
         </Container>
-      <p>Footer</p>
+      <Footer></Footer>
     </Router>
   );
 }
