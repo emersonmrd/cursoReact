de57631009ac3295b47bc6ff0f24903d373e3402
Emerson Martins <138334126+emersonmrd@users.noreply.github.com>
2024-03-28 14:15:06 -0300

    Página de edição de dados

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,10 +4,13 @@ import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+
 
 function App() {
   return (
@@ -20,6 +23,7 @@ function App() {
           <Route exact path="/projects" element={<Projects />} />
           <Route  exact path="/contact" element={<Contact />} />
           <Route  exact path="/newproject" element={<NewProject />} />
+          <Route  exact path="/project/:id" element={<Project />} />
         </Routes>
         </Container>
       <Footer></Footer>
